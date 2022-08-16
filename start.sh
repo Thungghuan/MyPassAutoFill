03d3f5e9ebc0ceb949c11f7148e86dd08f147261
Thungghuan <1092887426@qq.com>
2022-08-16 12:03:59 +0800

    chore: update script and README

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-echo "" > $PWD/mypasslog
+read -p "请确保你已更新过最新的数据！[yN] " yn
+case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) echo "请先在系统中填报/更新一次最新数据在使用本脚本:)"; exit;;
+    * ) exit ;;
+esac
+
+echo "" >> $PWD/mypasslog
 
 crontab -l > conf 2>/dev/null
 echo "30 8 * * * python3 $PWD/mypass.py >> $PWD/mypasslog 2>&1"  >> conf
