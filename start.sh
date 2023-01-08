3cff0896711d770e3a80ef9d8744e2371992fe71
Thungghuan <1092887426@qq.com>
2023-01-08 14:32:01 +0800

    chore: typo

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 read -p "请确保你已更新过最新的数据！[yN] " yn
 case $yn in
     [Yy]* ) break;;
-    [Nn]* ) echo "请先在系统中填报/更新一次最新数据在使用本脚本:)"; exit;;
-    * ) exit ;;
+    [Nn]* ) echo "请先在系统中填报/更新一次最新数据再使用本脚本:)"; exit;;
+    * ) exit;;
 esac
 
 echo "" >> $PWD/mypasslog
