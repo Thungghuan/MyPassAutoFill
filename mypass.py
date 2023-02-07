8d39384b988709288610f7344265cda76109dc1d
Thungghuan <1092887426@qq.com>
2023-02-07 16:23:32 +0800

    feat: refactor and add continuous check

--- mypass.py
+++ mypass.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 import re
 import requests
 import time
@@ -7,56 +8,81 @@ from des import str_enc
 from data import users
 
 
-def mypass_main(username, password):
-    session = requests.session()
+class MyPass:
+    def __init__(self, username: str, password: str) -> None:
+        self.session = requests.session()
+        self.session.trust_env = False
+        self.username = username
+        self.password = password
 
-    session.trust_env = False
+    def login(self):
+        redirect_login_page = self.session.get(
+            "https://enroll.scut.edu.cn/door/index.html"
+        )
+        lt_pattern = re.compile(r'<input.+name="lt"\s+value="([^"]+)"')
+        lt = re.search(lt_pattern, redirect_login_page.text).group(1)
 
-    redirect_login_page = session.get(
-        "https://enroll.scut.edu.cn/door/index.html")
-    lt_pattern = re.compile(r'<input.+name="lt"\s+value="([^"]+)"')
-    lt = re.search(lt_pattern, redirect_login_page.text).group(1)
+        login_post_param = {
+            "ul": len(self.username),
+            "pl": len(self.password),
+            "lt": lt,
+            "rsa": str_enc(self.username + self.password + lt, "1", "2", "3"),
+            "execution": "e1s1",
+            "_eventId": "submit",
+        }
 
-    login_post_param = {
-        'ul': len(username),
-        'pl': len(password),
-        'lt': lt,
-        'rsa': str_enc(username + password + lt, '1', '2', '3'),
-        'execution': 'e1s1',
-        '_eventId': 'submit'
-    }
+        self.session.post(
+            url="https://sso.scut.edu.cn/cas/login"
+            + "?service=https%3A%2F%2Fenroll.scut.edu.cn%2Fdoor%2Fhealth%2Fh5%2Fhealth.html'",
+            data=login_post_param,
+        )
 
-    login = session.post(
-        url="https://sso.scut.edu.cn/cas/login" +
-        "?service=https%3A%2F%2Fenroll.scut.edu.cn%2Fdoor%2Fhealth%2Fh5%2Fhealth.html'",
-        data=login_post_param
-    )
+    def get_health_data(self):
+        health_data_res = self.session.get(
+            "https://enroll.scut.edu.cn/door/health/h5/get"
+        )
+        self.health_data = json.loads(health_data_res.text)["data"]["healthRptInfor"]
+        self.last_submit_date = self.health_data["dRptDate"]
 
-    health_data_res = session.get(
-        "https://enroll.scut.edu.cn/door/health/h5/get")
+    @property
+    def is_continuous(self):
+        """
+        判断是否连续填报（上次填报是30天内）
+        """
+        today = date.today()
+        last_date = datetime.strptime(self.last_submit_date, "%Y-%m-%d").date()
+        return (today - last_date).days <= 30
 
-    health_data = json.loads(health_data_res.text)['data']['healthRptInfor']
+    def post_health_data(self):
+        post_health_data = {}
+        for key in self.health_data:
+            post_health_data[key] = self.health_data[key]
 
-    post_health_data = {}
-    for key in health_data:
-        post_health_data[key] = health_data[key]
+        add_data = self.session.post(
+            url="https://enroll.scut.edu.cn/door/health/h5/add", data=post_health_data
+        )
+        self.write_log(add_data.text)
 
-    add_data = session.post(
-        url='https://enroll.scut.edu.cn/door/health/h5/add',
-        data=post_health_data
-    )
-
-    with open(sys.path[0] + '/mypasslog', 'a') as f:
-        f.write(time.strftime("%Y-%m-%d %H:%M:%S",
-                time.localtime()) + ": " + "\n")
-        f.write("username: " + username + "\n")
-        f.write("result: " + add_data.text + '\n')
+    def write_log(self, result):
+        with open(sys.path[0] + "/mypasslog", "a") as f:
+            f.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ": " + "\n")
+            f.write("username: " + self.username + "\n")
+            f.write("result: " + result + "\n")
 
 
 def main():
     for user in users:
-        mypass_main(user["username"], user["password"])
+        mypass = MyPass(user["username"], user["password"])
+
+        mypass.login()
+        mypass.get_health_data()
+
+        if mypass.is_continuous:
+            mypass.post_health_data()
+        else:
+            print(f"上次填报的日期是{mypass.last_submit_date}, 已超过30天")
+            print("请先在系统重新填报一次后再使用本工具")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
